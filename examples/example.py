c1981a7bc5add60f67a39bbff6b97c7cb7c70434
Kacper Serewiś <kacper@kacperserewis.net>
2022-03-16 16:37:34 +0100

    add new example

--- examples/example.py
+++ examples/example.py
@@ -1,26 +1,22 @@
-from hyperx_keyboard_rgb.models.ksp_keyboard import KspKeyboard
-from hyperx_keyboard_rgb.helpers.hyperx_alloy_origins import COLOR_KEYBOARD_BUFFER, COLOR_KEY, KEYS, COLOR_KEYBOARD_PACKETS
+from time import sleep
+from hyperx_keyboard_rgb.keyboard import KeyboardHelper, Keyboard
+from hyperx_keyboard_rgb.exceptions.device_not_open import DeviceNotOpen
+from hyperx_keyboard_rgb.helpers.hyperx_alloy_origins import KEYS
+from hyperx_keyboard_rgb.models.ksp_color import KspColor
 
-hid_path = input("Path:")
 
+found_sub = KeyboardHelper.get_subdevice(0x951, 0x16e5, 0)
 
-# Define keyboard
-keyboard = KspKeyboard(hid_path.encode("ASCII"))
+kb = Keyboard.from_ksp_hid_subdevice(found_sub)
+
 try:
-    keyboard.connect()
-except Exception as e:
-    print("Can't connect!", e)
+    kb.connect()
+except DeviceNotOpen:
+    print("Can't open device :(")
     exit(1)
 
 
-# Setup keyboard colors
-keyboard_buffer = COLOR_KEYBOARD_BUFFER()
-for i in ['K', 'S', 'P']:
-    keyboard_buffer.replace_at_offset(COLOR_KEY(255, 0, 0), KEYS[i])
-
-try:
-    while True:
-        for packet in COLOR_KEYBOARD_PACKETS(keyboard_buffer):
-            keyboard.send_feature_report(packet.get(), True)
-except KeyboardInterrupt:
-    exit()
+kb.start_thread()
+for key in KEYS.keys():
+    kb.set_color(KEYS[key], KspColor(255, 0, 0))
+    sleep(0.1)
